5e58aa12f0eaba76081572994b2d2c1b195fd777
Nata <melvia.nr@gmail.com>
2022-11-08 22:07:47 +0500

    resolver without data handle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {HelpComponent} from "./help/help.component";
+import {AboutComponent} from "./about/about.component";
+import {DataService} from "./data.service";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HeaderComponent,
+    resolve: {
+      employees: DataService
+    }},
+  {path: "about", component: AboutComponent},
+  {path: "help", component: HelpComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {AboutComponent} from "./about/about.component";
+import { HelpComponent } from './help/help.component';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HelpComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
